1035359823cea444e0711c9803bb12bc116967c4
Pierre Gabory <piergabory@gmail.com>
2018-11-29 23:04:39 +0100

    Shader Documentation

--- includes/GraphicsEngine/Object/Material/Material.hpp
+++ includes/GraphicsEngine/Object/Material/Material.hpp
@@ -1,36 +1,50 @@
-//
-//  Material.hpp
-//  xcode target
-//
-//  Created by Pierre Gabory on 28/11/2018.
-//  Copyright © 2018 Pierre Gabory. All rights reserved.
-//
-
+/**
+ * Material.hpp
+ *
+ * IMAC 2 Project CG CPP
+ *
+ */
 #ifndef Material_hpp
 #define Material_hpp
 
 #include "Texture.hpp"
 #include "PerspectiveShaderProgram.hpp"
 
+/**
+ * MATERIAL CLASS
+ *
+ * Controls the appearance of 3D objects
+ * Manages texture and shaders.
+ */
 namespace GraphicsEngine {
+    
     class Material {
+        
     private:
+        // texture pointer
+        // TODO: Allow multiple textures
         Texture* m_texture;
+        
+        // shader pointer
+        // perspective shader handles projection
         PerspectiveShaderProgram* m_shader;
         
-    public:
-        Material(Texture *texture, PerspectiveShaderProgram *shader):
-        m_texture(texture), m_shader(shader) {
-            texture->linkTo(*shader);
-        };
         
+    public:
+        // constructor
+        Material(Texture *texture, PerspectiveShaderProgram *shader);
+        
+        // destructor
         ~Material() {};
         
+        // getters
         inline PerspectiveShaderProgram* shader() { return m_shader; }
         inline Texture* texture() { return m_texture; }
         
+        // activate the shader program and select the textures
         void apply() const;
     };
+    
 }
 
 #endif /* Material_hpp */
--- src/GraphicsEngine/Object/Material/Shader/PerspectiveShaderProgram.cpp
+++ src/GraphicsEngine/Object/Material/Shader/PerspectiveShaderProgram.cpp
@@ -1,15 +1,12 @@
-////
-////  PerspectiveShaderProgram.cpp
-////  xcode target
-////
-////  Created by Pierre Gabory on 28/11/2018.
-////  Copyright © 2018 Pierre Gabory. All rights reserved.
-////
+/**
+ * PerspectiveShaderProgram.hpp
+ */
 #include "PerspectiveShaderProgram.hpp"
 
 namespace GraphicsEngine {
-    PerspectiveShaderProgram::PerspectiveShaderProgram(const char* vertexShaderSourcePath, const char* fragmentShaderSourcePath) : ShaderProgram(vertexShaderSourcePath, fragmentShaderSourcePath)
-    {
+    
+    PerspectiveShaderProgram::PerspectiveShaderProgram(const char* vertexShaderSourcePath, const char* fragmentShaderSourcePath) :
+    ShaderProgram(vertexShaderSourcePath, fragmentShaderSourcePath) {
         findUniforms();
     }
     
@@ -17,12 +14,17 @@ namespace GraphicsEngine {
         m_uniformModelViewMatrix = glGetUniformLocation(m_glProgramIdentifier, "uMVPMatrix");
         m_uniformNormalMatrix = glGetUniformLocation(m_glProgramIdentifier, "uMVMatrix");
         m_uniformModelViewProjectionMatrix = glGetUniformLocation(m_glProgramIdentifier, "uNormalMatrix");
-        m_uniformTextureSampler = glGetUniformLocation(m_glProgramIdentifier, "uTexture");
     }
     
     void PerspectiveShaderProgram::setUniformMatrices(const glm::mat4 &modelView, const glm::mat4 &projection) {
+        // modelview = modelview
         glUniformMatrix4fv(m_uniformModelViewMatrix, 1, false, glm::value_ptr(modelView));
+        
+        // Normal = transversed( inverted( modelview ))
+        // the 'true' parameter asks the method to do the transvertion of the input matrix.
         glUniformMatrix4fv(m_uniformNormalMatrix, 1, true, glm::value_ptr(glm::inverse(modelView)));
+        
+        // modelviewProjection = modelview * projection
         glUniformMatrix4fv(m_uniformModelViewProjectionMatrix, 1, false, glm::value_ptr(modelView * projection));
     }
 }
