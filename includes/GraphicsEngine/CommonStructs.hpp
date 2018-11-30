a3f7ecd3ea84d1d902a71668a8185e59b1869262
Pierre Gabory <piergabory@gmail.com>
2018-11-30 21:46:18 +0100

    Common structs and Frameworks documentation

--- includes/GraphicsEngine/CommonStructs.hpp
+++ includes/GraphicsEngine/CommonStructs.hpp
@@ -1,10 +1,9 @@
- //
- //  CommonStructs.hpp
- //  product
- //
- //  Created by Pierre Gabory on 17/11/2018.
- //
-
+/**
+ * CommonStructs.hpp
+ *
+ * IMAC 2 Project CG CPP
+ *
+ */
  #ifndef CommonStructs_hpp
  #define CommonStructs_hpp
 
@@ -12,15 +11,19 @@
 
  namespace GraphicsEngine {
     
+     /**
+      * VERTEX STRUCT
+      * holds vertex data for 3D meshes.
+      */
      struct Vertex {
+         // properties
          glm::vec3 m_position;
          glm::vec3 m_normal;
          glm::vec2 m_textureCoordinates;
          
+         // constructor
          Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 texture):
-            m_position(position), m_normal(normal), m_textureCoordinates(texture)
-         {}
-         
+            m_position(position), m_normal(normal), m_textureCoordinates(texture) {}
      };
     
  }
--- includes/GraphicsEngine/Frameworks.hpp
+++ includes/GraphicsEngine/Frameworks.hpp
@@ -1,29 +1,51 @@
+/**
+ * Frameworks.hpp
+ *
+ * IMAC 2 Project CG CPP
+ *
+ */
+
 #ifndef Frameworks_hpp
 #define Frameworks_hpp
 
-#define GL_SILENCE_DEPRECATION
 
+/**
+ * Includes for different operating systems
+ */
+
+// Mac OS includes
+// ------------------------------------------------
+// disable MacOS Mojave OpenGL deprecation warnings
+#define GL_SILENCE_DEPRECATION
 #ifdef __APPLE__
-    // Mac OS
+
+    // SDL library and extensions
     #include <SDL2/SDL.h>
     #include <SDL2_image/SDL_image.h>
+
+    // GLEW
     #include <GL/glew.h>
+
+    // OpenGL
     #include <OpenGL/gl.h>
     #include <OpenGL/glu.h>
 
 
-//    #define glGenVertexArrays glGenVertexArraysAPPLE
-//    #define glBindVertexArray glBindVertexArrayAPPLE
-//    #define glDeleteVertexArrays glDeleteVertexArraysAPPLE
-
+// Linux includes
+// ------------------------------------------------
 #else
-    // Linux
-    #include <GL/glew.h>
+
+    // SDL library and extensions
     #include <SDL2/SDL.h>
     #include <SDL2/SDL_image.h>
+
+    // GLEW
+    #include <GL/glew.h>
+
+    // OpenGL
     #include <GL/gl.h>
     #include <GL/glu.h>
 
 #endif
 
-#endif
+#endif // end: Frameworks_hpp
