4200778b63d13ceb80e9eb4e299c252f960b8983
Pierre Gabory <piergabory@gmail.com>
2018-11-30 21:32:17 +0100

    Camera Documentation

--- includes/GraphicsEngine/Camera.hpp
+++ includes/GraphicsEngine/Camera.hpp
@@ -1,9 +1,9 @@
-//
-//  Camera.hpp
-//  xcode target
-//
-//  Created by Pierre Gabory on 28/11/2018.
-//
+/**
+ * Cameras.hpp
+ *
+ * IMAC 2 Project CG CPP
+ *
+ */
 
 #ifndef Camera_hpp
 #define Camera_hpp
@@ -13,31 +13,51 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+/**
+ * CAMERA CLASS
+ *
+ * Facade to the glm projection matrix
+ * provides the camera's projection matrix
+ * and attributes to rotate and move the simulated camera
+ */
+
 namespace GraphicsEngine {
+    
     class Camera {
+        
     private:
+        // Spatial properties of the simulated camera
         glm::vec3 m_position;
         glm::vec3 m_orientation;
         float m_angle;
         
+        // camera lense properties
         float m_fieldOfView;
         float m_aspectRatio;
         
+        // rendering distance
+        // also defines the sampling of the zbuffer
         float m_minRenderingDistance;
         float m_maxRenderingDistance;
         
+        // keeps the matrix around so we only have to
+        // compute it when the camera moves
         glm::mat4 m_projectionMatrix;
         
+        // updates the above matrix
         void updateProjectionMatrix();
         
     public:
+        // constructor
         Camera(const float fieldOfView = 90,
                const float aspect = 4/3,
                const float min = 0.1f,
                const float max = 100.f);
         
+        // destructor
         ~Camera() {}
         
+        // getter
         inline const glm::mat4 projectionMatrix() const { return m_projectionMatrix; }
     };
 }
--- src/GraphicsEngine/Camera.cpp
+++ src/GraphicsEngine/Camera.cpp
@@ -1,27 +1,28 @@
-//
-//  Camera.cpp
-//  xcode target
-//
-//  Created by Pierre Gabory on 28/11/2018.
-//  Copyright © 2018 Pierre Gabory. All rights reserved.
-//
+/**
+ * Cameras.hpp
+ */
 
 #include "Camera.hpp"
 
 namespace GraphicsEngine {
     
     void Camera::updateProjectionMatrix() {
+        // compute camera projection
         m_projectionMatrix = glm::perspective(glm::radians(m_fieldOfView),
                                               m_aspectRatio,
                                               m_minRenderingDistance,
                                               m_maxRenderingDistance);
+        
+        // move the camera
         m_projectionMatrix = glm::translate(glm::mat4(1), m_position) * m_projectionMatrix;
         m_projectionMatrix = glm::rotate(glm::mat4(1), m_angle, m_orientation) * m_projectionMatrix;
     }
     
     
+    // constructor
     Camera::Camera(const float fieldOfView, const float aspect, const float min, const float max) :
     m_angle(0), m_fieldOfView(fieldOfView), m_aspectRatio(aspect), m_minRenderingDistance(min), m_maxRenderingDistance(max) {
+        // defines m_projectionMatrix
         updateProjectionMatrix();
     }
     
