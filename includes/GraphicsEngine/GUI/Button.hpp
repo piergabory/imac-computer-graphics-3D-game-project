bed2773719c4d160a0d5db05cc60ec60c154ad19
Pierre Gabory <piergabory@gmail.com>
2018-12-18 17:30:53 +0100

    New button class

---
+++ includes/GraphicsEngine/GUI/Button.hpp
@@ -0,0 +1,19 @@
+//
+//  Button.hpp
+//  xcode target
+//
+//  Created by Pierre Gabory on 18/12/2018.
+//  Copyright © 2018 Pierre Gabory. All rights reserved.
+//
+
+#ifndef Button_hpp
+#define Button_hpp
+
+#include "Object2D.hpp"
+#include "EventObserver.hpp"
+
+class Button : public Object2D {
+
+}
+
+#endif /* Button_hpp */
---
+++ src/GraphicsEngine/GUI/Button.cpp
@@ -0,0 +1,9 @@
+//
+//  Button.cpp
+//  xcode target
+//
+//  Created by Pierre Gabory on 18/12/2018.
+//  Copyright © 2018 Pierre Gabory. All rights reserved.
+//
+
+#include "Button.hpp"
