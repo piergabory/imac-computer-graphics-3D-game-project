5735ba2b6b10a17e789dd553483bffe3c46e37d4
Pierre Gabory <piergabory@gmail.com>
2018-12-22 17:26:48 +0100

    Player Doc

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,33 +1,45 @@
+/**
+ * Player.cpp
+ */
 #include "Player.hpp"
 
-Player::Player(std::shared_ptr<GraphicsEngine::Object3D> obj):
-    m_characterModel(obj),
-    m_jumpingAnimation(GraphicsEngine::makeBounceAnimation(m_characterModel, JUMP_FRAMETIME, JUMP_HEIGHT)),
-    m_crouchingAnimation(GraphicsEngine::makeCrouchAnimation(m_characterModel, CROUCH_FRAMETIME, CROUCH_HEIGHT)),
-    m_standingAnimation(GraphicsEngine::makeUnCrouchAnimation(m_characterModel, CROUCH_FRAMETIME, CROUCH_HEIGHT)),
-    m_moveToLeftLaneAnimation(GraphicsEngine::makeLinearTranslation(m_characterModel, TRANSLATE_FRAMETIME, glm::vec3(-LANE_WIDTH,0.f,0.f))),
-    m_moveToMiddleLaneAnimation(GraphicsEngine::makeLinearTranslation(m_characterModel, TRANSLATE_FRAMETIME, glm::vec3(0.f,0.f,0.f))),
-    m_moveToRightLaneAnimation(GraphicsEngine::makeLinearTranslation(m_characterModel, TRANSLATE_FRAMETIME, glm::vec3(LANE_WIDTH,0.f,0.f)))
-{}
 
 void Player::jump() {
+    // Player can only jump when standing
     if (m_status != Status::STANDING) return;
+
+    // when player reaches the ground, reset status to Standing
     m_jumpingAnimation.whenDone([=]() -> void { m_status = Status::STANDING; });
+
+    // makes player
     m_status = Status::JUMPING;
     m_jumpingAnimation.begin();
 }
 
+
+// toggles crouching
 void Player::crouch() {
+    // crouching
     if (m_status == Status::STANDING) {
         m_status = Status::CROUCHING;
         m_crouchingAnimation.begin();
-    } else if (m_status == Status::CROUCHING){
+    }
+
+    // standing
+    else if (m_status == Status::CROUCHING){
         m_status = Status::STANDING;
         m_standingAnimation.begin();
     }
 }
 
+
+
 void Player::move(const Direction &direction) {
+    // check if direction is a valid enumeration
+    assert((direction != Direction::LEFT ||  direction != Direction::RIGHT) && "Unknown Position");
+
+    // If in the middle move to the left/right
+    // else move to the center or dont move.
     switch (m_position) {
         case Position::LEFT:
             if(direction == Direction::RIGHT) {
@@ -54,7 +66,29 @@ void Player::move(const Direction &direction) {
             }
             break;
 
+        // When position is a non-defined enum.
         default: assert(false && "Unknown Position");
     }
-};
+}
+
+
+
+// constructor
+Player::Player(std::shared_ptr<GraphicsEngine::Object3D> obj):
+// init model
+m_characterModel(obj),
+
+// jumping animation
+m_jumpingAnimation(GraphicsEngine::makeBounceAnimation(m_characterModel, JUMP_FRAMETIME, JUMP_HEIGHT)),
+
+// crouching animations
+m_crouchingAnimation(GraphicsEngine::makeCrouchAnimation(m_characterModel, CROUCH_FRAMETIME, CROUCH_HEIGHT)),
+m_standingAnimation(GraphicsEngine::makeUnCrouchAnimation(m_characterModel, CROUCH_FRAMETIME, CROUCH_HEIGHT)),
+
+// movement animations
+m_moveToLeftLaneAnimation(GraphicsEngine::makeLinearTranslation(m_characterModel, TRANSLATE_FRAMETIME, glm::vec3(-LANE_WIDTH,0.f,0.f))),
+m_moveToMiddleLaneAnimation(GraphicsEngine::makeLinearTranslation(m_characterModel, TRANSLATE_FRAMETIME, glm::vec3(0.f,0.f,0.f))),
+m_moveToRightLaneAnimation(GraphicsEngine::makeLinearTranslation(m_characterModel, TRANSLATE_FRAMETIME, glm::vec3(LANE_WIDTH,0.f,0.f)))
+{}
+
 
