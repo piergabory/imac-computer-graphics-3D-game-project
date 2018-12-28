000eca26a7237fcfbe5f8bc7b33283e5748e2398
Pierre Gabory <piergabory@gmail.com>
2018-12-28 23:20:23 +0100

    Terrain Doc

--- includes/Model/Terrain.hpp
+++ includes/Model/Terrain.hpp
@@ -3,6 +3,7 @@
  *
  * IMAC 2 Project CG CPP
  */
+
 #ifndef Terrain_hpp
 #define Terrain_hpp
 
@@ -13,6 +14,8 @@
 
 namespace GameModel {
 
+    /// \brief Cadinal Direction enumeration
+    /// to represent chunk orientation
     enum class CardinalDirections : int {
         NORTH = 0,
         EAST = 90,
@@ -20,34 +23,66 @@ namespace GameModel {
         WEST = -90
     };
 
+
+    /**
+     * TERRAIN CLASS
+     *
+     * \brief Aggregates chunks.
+     * Points the active chunk.
+     */
     class Terrain {
     private:
+
+        /// \brief Number of chunks behind the player
+        /// Chunk count still around after being visited by the player.
         const uint m_CHUNK_COUNT_AFTER_PLAYER = 5;
+
+        /// \brief Chunk container
+        /// Dequeue allows random access (to get active chunk) and quick front/back edits
         std::deque< std::unique_ptr<Chunk> > m_chunks;
+
+        /// \brief Angle in randians of the next chunk to be added at the end of the terrain.
         float m_nextLoadedChunkOrientation = 0;
+
+        /// \brief Position of the next chunk to be added
         glm::vec3 m_nextLoadedChunkPosition = glm::vec3(0.f,0.f,0.f);
 
-        inline const std::unique_ptr<Chunk>& activeChunk() const { return m_chunks[m_CHUNK_COUNT_AFTER_PLAYER]; }
 
+        /// \brief active chunk getter.
+        inline const std::unique_ptr<Chunk>& activeChunk() const { return m_chunks[m_CHUNK_COUNT_AFTER_PLAYER]; }
         inline std::unique_ptr<Chunk>& activeChunk() { return m_chunks[m_CHUNK_COUNT_AFTER_PLAYER]; }
 
+
     public:
+        /// \brief destroys old chunks and shifts to the next one.
         inline void nextChunk() {
             m_chunks.pop_front();
             activeChunk()->onEnter();
         }
 
+        /// \brief orientation getter
+        /// Converts radian degree to Cardinal direction
         const CardinalDirections facing() const;
 
+        /// \brief test player when entering the chunk.
         void enterChunk(Player &player);
+
+        /// \brief test player on current chunk.
         void testAction(Player &player);
 
+        /// \brief push new chunks in the container
+        /// Places the objects in the scene.
         void loadChunk(Chunk *chunkToLoad);
 
+        /// \brief displaces all objects in the scene in the direction of the active chunk.
+        /// Creates the illusion of movement under the player.
         void progress(const float progress);
 
+
+        // constructor
         Terrain() {}
-        
+
+        // destructor
         ~Terrain() {}
     };
 
--- src/Model/Terrain.cpp
+++ src/Model/Terrain.cpp
@@ -4,27 +4,37 @@
 #include "Terrain.hpp"
 
 namespace GameModel {
+
     void Terrain::enterChunk(Player &player) {
         activeChunk()->entityAt(player.position())->onEnter();
     }
+
+
 
     void Terrain::testAction(Player &player) {
         activeChunk()->entityAt(player.position())->action();
     }
 
+
+
     void Terrain::loadChunk(Chunk *chunkToLoad) {
-        glm::vec3 up(0.f,1.f,0.f);
         chunkToLoad->rotate(m_nextLoadedChunkOrientation);
         chunkToLoad->translate(m_nextLoadedChunkPosition);
-        m_nextLoadedChunkPosition += glm::vec3(glm::rotate(glm::mat4(1), m_nextLoadedChunkOrientation, up) * glm::vec4(chunkToLoad->exitPosition(), 1.f));
+
+        // updates coordinates for the next chunk
+        // based on the output of the new chunk
+        m_nextLoadedChunkPosition += glm::vec3(glm::rotate(glm::mat4(1), m_nextLoadedChunkOrientation, glm::vec3(0.f,1.f,0.f)) * glm::vec4(chunkToLoad->exitPosition(), 1.f));
         m_nextLoadedChunkOrientation += chunkToLoad->exitOrientation();
         m_chunks.emplace_back(chunkToLoad);
     }
+
+
 
     void Terrain::progress(const float progress){
         // creates a rotation matrix from the front's chunk orientation and creates the translation vector
         glm::vec3 translationVector = glm::vec3(glm::rotate(glm::mat4(1), m_chunks.front()->orientation(), glm::vec3(0,1,0)) * glm::vec4(-progress * m_chunks.front()->exitPosition(), 1.f));
 
+        // applies translation on all entities
         for (std::deque<std::unique_ptr<Chunk>>::iterator chunkIterator = m_chunks.begin(); chunkIterator != m_chunks.end(); ++chunkIterator) {
             (*chunkIterator)->translate(translationVector);
         }
@@ -32,8 +42,11 @@ namespace GameModel {
         m_nextLoadedChunkPosition += translationVector;
     }
 
+
+
     const CardinalDirections Terrain::facing() const {
         int orientation = ((int)glm::degrees(activeChunk()->orientation()) % 180);
         return CardinalDirections(orientation);
     }
+
 }
